75d702a8336a2c74bba9ac8fe23e1f858899e1fa
James Barrow <james.barrow@sitecore.com>
2020-07-03 21:27:31 +1000

    ImageGallery support for CDN

---
+++ src/Feature/XA.MediaExtensions/code/Media/Services/MediaItemService.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.Multisite.LinkManagers;
+
+namespace MadCill.XA.Feature.MediaExtensions.Media.Services
+{
+    public class MediaItemService : Sitecore.XA.Feature.Media.Services.MediaItemService
+    {
+        public MediaItemService(Sitecore.Abstractions.BaseMediaManager mediaManager) 
+            : base(mediaManager)
+        {
+        }
+
+        public override string GetImageFieldTargetUrl(Item item, ID fieldId)
+        {
+            string str = string.Empty;
+            ImageField field = (ImageField)item.Fields[fieldId];
+            if (field?.MediaItem != null)
+            {
+                str = this.MediaManager.GetMediaUrl(new MediaItem(field.MediaItem));
+                if (!Settings.GetBoolSetting("Media.AlwaysIncludeServerUrl", false))
+                {
+                    str = StringUtil.EnsurePrefix('/', str);
+                }                
+            }
+            return str;
+        }
+    }
+}
---
+++ src/Feature/XA.MediaExtensions/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MadCill.Feature.XA.MediaExtensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MadCill.Feature.XA.MediaExtensions")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f09a62b3-30fc-4f7d-8a02-fdbb91cb88c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
